39d718e3bdb2e6959a0ebe72e066d3c83cffae11
Lucas <lucas.santini@nddigital.com.br>
2020-10-29 10:41:59 -0300

    remoção de comentários

--- src/basic/index-pdf.js
+++ src/basic/index-pdf.js
@@ -1,5 +1,12 @@
 const puppeteer = require('puppeteer');
 
 const pdf = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-}
+    await page.goto('https://github.com/lucasmsantini');
+    await page.pdf({path:'meugit.pdf', format: 'A4'});
+    await browser.close();
+};
+
+pdf();

9307a0c7da594f7f2b5e3ddf9ccc9c4999a8ac60
Lucas <lucas.santini@nddigital.com.br>
2020-10-29 10:38:17 -0300

    pdf

---
+++ src/basic/index-pdf.js
@@ -0,0 +1,5 @@
+const puppeteer = require('puppeteer');
+
+const pdf = async () => {
+
+}
--- src/basic/index.js
+++ src/basic/index.js
@@ -1,11 +1,14 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+const printScreen = async  () => {
     const browser = await puppeteer.launch({
-        deadless:false
+        //headless:false
     });
     const page = await browser.newPage();
+    await page.setViewport({width: 1920, height:1080})
     await page.goto('https://github.com/lucasmsantini');
     await page.screenshot({path:'meugit.png'})
     await browser.close();
-})();
+};
+
+printScreen();
